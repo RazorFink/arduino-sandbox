37efc5d86b12d06b72ac2b4f79023c51bf0193df
Matt Howe <mhowemegis@gmail.com>
2013-03-16 12:02:12 -0400

    w/ serial comm

--- IREmitterDetector/IREmitterDetector.ino
+++ IREmitterDetector/IREmitterDetector.ino
@@ -5,7 +5,7 @@ void setup(){
   pinMode(9, OUTPUT);  //IR emitter enable
   pinMode(13, OUTPUT); ///built in LED
   digitalWrite(14, HIGH); // enable pullup resistor on analog input A0/D14
-//  Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -16,7 +16,11 @@ void loop() {
   threshold = 1023 - analogRead(1); // get threshold value
   infrared = 1023 - analogRead(0); // get voltage from detector circuit
   digitalWrite(9, LOW); // disable infrared emitter
-  
+  Serial.print("threshold:");
+  Serial.print(threshold);
+  Serial.print(", IR:");
+  Serial.println(infrared);
+  delay(200);
   if(infrared > threshold) {
     digitalWrite(13, HIGH); // LED on = IR above threshold 
   } else {
