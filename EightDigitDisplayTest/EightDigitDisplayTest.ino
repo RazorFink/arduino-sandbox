bc59a99695ce04cf816e82b2f62fec8cc21161cc
Matt Howe <mhowemegis@gmail.com>
2013-03-23 10:24:42 -0400

    Demo for the new 8 digit display

--- EightDigitDisplayTest/EightDigitDisplayTest.ino
+++ EightDigitDisplayTest/EightDigitDisplayTest.ino
@@ -2,15 +2,11 @@
 // control 8 * 7-segment displays over SPI using LedControl library
 
 #include "LedControl.h"
-#define PIN_DIN = 12
-#define PIN_CLK = 11
-#define PIN_LOAD = 10
+#define PIN_DIN 12
+#define PIN_CLK 11
+#define PIN_LOAD 10
 
-LedControl lc = LedControl(12, 11, 10, 1);
-
-//char message[8] = {'b','0','0','b','1','E','5'};
-String message = "b00b1E5";
-byte delayTime = 250;
+LedControl lc = LedControl(PIN_DIN, PIN_CLK, PIN_LOAD, 1);
 
 void setup() {
   lc.shutdown(0, false);
@@ -24,11 +20,16 @@ void displayMessage(byte startDigit, String message) {
   }  
 }
 
+void counter(byte digit) {
+   lc.setDigit(0,digit,0,false);
+   for (int i = 1; i < 11; i++) {
+    if(digit){
+      counter(digit-1);
+    }
+    lc.setDigit(0, digit, i, false); 
+  }
+}
 
 void loop() { 
-  for (int d = 0; d < 8 + message.length(); d++){
-    lc.clearDisplay(0);
-    displayMessage(d, message);
-    delay(delayTime);
-  }
+  counter(7);
 }
